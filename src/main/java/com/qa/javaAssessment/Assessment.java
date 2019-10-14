75267c0409913ca7a6cd315d28c396c4f018f104
Matthew <matthewdkemp91@gmail.com>
2019-10-14 23:23:31 +0100

    5 of 8 tests passed

--- src/main/java/com/qa/javaAssessment/Assessment.java
+++ src/main/java/com/qa/javaAssessment/Assessment.java
@@ -12,14 +12,14 @@ public class Assessment {
 
 	public String multChar(String input) {
 		String word = "";
-		for (int i = 0; i< input.length(); i++) {
-			word += input.substring(i, i+1);
-			word += input.substring(i, i+1);
-			word += input.substring(i, i+1);
+		for (int i = 0; i < input.length(); i++) {
+			word += input.substring(i, i + 1);
+			word += input.substring(i, i + 1);
+			word += input.substring(i, i + 1);
 		}
 		return word;
 	}
-	
+
 	// Return the string (backwards) that is between the first and last appearance
 	// of "bert"
 	// in the given string, or return the empty string "" if there is not 2
@@ -47,20 +47,32 @@ public class Assessment {
 	// evenlySpaced(4, 60, 9) ==> false
 
 	public boolean evenlySpaced(int a, int b, int c) {
-		return false;
+		if ((a - b) == (b - c) || (a - c) == (b - c) || (a - b) == (c - a)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	// Given a string and an int n, return a string that removes n letters from the 'middle' of the string.
-	// The string length will be at least n, and be odd when the length of the input is odd.
+	// Given a string and an int n, return a string that removes n letters from the
+	// 'middle' of the string.
+	// The string length will be at least n, and be odd when the length of the input
+	// is odd.
 
 	// nMid("Hello", 3) ==> "Ho"
 	// nMid("Chocolate", 3) ==> "Choate"
 	// nMid("Chocolate", 1) ==> "Choclate"
 
 	public String nMid(String input, int a) {
-		return "";
-	}
+		int midPoint = input.length() / 2;
 
+		int LHS = a / 2;
+		int RHS = a - LHS;
+
+		String result = input.substring(0, midPoint - LHS) + input.substring(midPoint + RHS);
+
+		return result;
+	}
 
 	// Given a string, return the length of the largest "block" in the string.
 	// A block is a run of adjacent chars that are the same.
@@ -72,57 +84,61 @@ public class Assessment {
 	public int superBlock(String input) {
 		int num = 0;
 		int count = 1;
-	    char s = ' ';
-	    for (int i = 0; i < input.length(); i++) {
-	        char c = input.charAt(i);
-	        if (c == s) {
-	            count++;
-	            if (count > num) {
-	            	num = count;
-	            }
-	        } 
-	        else {
-	            count = 1;
-	            if (count > num) {
-	            	num = count;
-	            }
-	        }
-	        s = c;
-	    }   
+		char s = ' ';
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (c == s) {
+				count++;
+				if (count > num) {
+					num = count;
+				}
+			} else {
+				count = 1;
+				if (count > num) {
+					num = count;
+				}
+			}
+			s = c;
+		}
 		return num;
 	}
-	
-	//given a string - return the number of times "am" appears in the String ignoring case -
-	// BUT ONLY WHEN the word "am" appears without being followed or proceeded by other letters
+
+	// given a string - return the number of times "am" appears in the String
+	// ignoring case -
+	// BUT ONLY WHEN the word "am" appears without being followed or proceeded by
+	// other letters
 	//
-	//amISearch("Am I in Amsterdam") ==> 1
-	//amISearch("I am in Amsterdam am I?") ==> 2
-	//amISearch("I have been in Amsterdam") ==> 0
+	// amISearch("Am I in Amsterdam") ==> 1
+	// amISearch("I am in Amsterdam am I?") ==> 2
+	// amISearch("I have been in Amsterdam") ==> 0
 
 	public int amISearch(String arg1) {
 		return -1;
-		
+
 	}
-	
-	//given a number 
+
+	// given a number
 	// if this number is divisible by 3 return "fizz"
 	// if this number is divisible by 5 return "buzz"
-	// if this number is divisible by both 3  and 5return "fizzbuzz"
+	// if this number is divisible by both 3 and 5return "fizzbuzz"
 	//
-	//fizzBuzz(3) ==> "fizz"
-	//fizzBuzz(10) ==> "buzz"
-	//fizzBuzz(15) ==> "fizzbuzz"
-	
+	// fizzBuzz(3) ==> "fizz"
+	// fizzBuzz(10) ==> "buzz"
+	// fizzBuzz(15) ==> "fizzbuzz"
+
 	public String fizzBuzz(int arg1) {
-		String word="";
-		if(arg1%3==0 && arg1%5!=0) word="fizz";
-		if(arg1%5==0 && arg1%3!=0) word="buzz";
-		if(arg1%3==0 && arg1%5==0) word="fizzbuzz";
+		String word = "";
+		if (arg1 % 3 == 0 && arg1 % 5 != 0)
+			word = "fizz";
+		if (arg1 % 5 == 0 && arg1 % 3 != 0)
+			word = "buzz";
+		if (arg1 % 3 == 0 && arg1 % 5 == 0)
+			word = "fizzbuzz";
 		return word;
 	}
-	
-	//Given a string split the string into the individual numbers present
-	//then add each digit of each number to get a final value for each number
+
+	// Given a string split the string into the individual numbers present
+	// then add each digit of each number to get a final value for each number
 	// String example = "55 72 86"
 	//
 	// "55" will = the integer 10
@@ -131,16 +147,15 @@ public class Assessment {
 	//
 	// You then need to return the highest vale
 	//
-	//largest("55 72 86") ==> 14
-	//largest("15 72 80 164") ==> 11
-	//largest("555 72 86 45 10") ==> 15
-	
+	// largest("55 72 86") ==> 14
+	// largest("15 72 80 164") ==> 11
+	// largest("555 72 86 45 10") ==> 15
+
 	public int largest(String arg1) {
-		int largest=0;
-		int counter=0;
-		int length=0;
-		
-		
+		int largest = 0;
+		int counter = 0;
+		int length = 0;
+
 		return largest;
 	}
 }
